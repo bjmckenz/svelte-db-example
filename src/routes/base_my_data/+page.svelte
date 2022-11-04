3e15a3ea44015cfe9997799cf187822c73073f34
Bruce McKenzie <bjmckenz@gmail.com>
2022-11-04 02:26:00 -0700

    DB version working

--- src/routes/base_my_data/+page.svelte
+++ src/routes/base_my_data/+page.svelte
@@ -1,61 +1,65 @@
-
 <script>
-import WordTable from './WordTable.svelte';
+	import WordTable from './WordTable.svelte';
 
-export let newval = '';
+	let newval;
 
-let wordlist = [{ word:'alpha'},{word:'banana'}];
+	/** @type {import('./$types').PageData} */
+	export let data;
 
-function refresh() {
-	// open Chrome debugger to see this
-  console.log("clicked refresh!");
-	location.reload();
-}
+	function refresh() {
+		// open Chrome debugger to see this message
+		console.log('clicked refresh!');
+		location.reload();
+	}
 
-function add() {
-	// open Chrome debugger to see this
-  console.log("clicked add!. would add '"+newval+"'");
-}
+	async function add() {
+		// open Chrome debugger to see this message
+		console.log("clicked add!. would add '" + newval + "'");
+		console.log({ data });
 
+		const formdata = new FormData();
+		formdata.append('word', newval);
+
+		// this matches an action in +page.server.js
+		const response = await fetch('?/insertWord', {
+			method: 'POST',
+			body: formdata
+		});
+
+		// a POST redirects to the new resource
+		await response.json();
+	}
+
+	// show that we loaded the data from the DB "at first page render"
+	console.log('first ', { data });
 </script>
 
-<div class=title>Base My Data!</div>
-<div class=note>Loads from DB when shown. Can add to DB. Click Refresh to reload from DB</div>
+<div class="title">Base My Data!</div>
+<div class="note">Loads from DB when shown. Can add to DB. Click Refresh to reload from DB</div>
 
 <div>
-	<WordTable words={wordlist} />
+	<WordTable words={data.words} />
 </div>
+<form>
+	<div>
+		<input bind:value={newval} />
+		<input class="btn" type="button" on:click={add} value="Add This Entry" />
+	</div>
 
-<div>
-	<input bind:value={newval}/>
-			<input
-				class="btn"
-				type="button"
-				on:click={add}
-				value="Add This Entry"
-			/>
-		</div>
-
-
-<div>
-			<input
-				class="btn"
-				type="button"
-				on:click={refresh}
-				value="Refresh Page"
-			/>
-		</div>
+	<div>
+		<input class="btn" type="button" on:click={refresh} value="Refresh Page" />
+	</div>
+</form>
 
 <style>
-  .title	{
+	.title {
 		text-align: center;
-    font-size: 2vw;
+		font-size: 2vw;
 		font-family: Arial;
-}
-.note	{
+	}
+	.note {
 		text-align: center;
-    font-size: 1vw;
+		font-size: 1vw;
 		font-family: Arial;
-}
-
+	}
 </style>
